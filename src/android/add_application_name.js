e645e209e473b2ac42bc002b59415a9090482c7b
wangzhengbo2 <wangzhengbo2@jd.com>
2019-10-08 18:25:34 +0800

    context.requireCordovaModule(fs)和context.requireCordovaModule(path)替换成require(fs)和require(path)

--- src/android/add_application_name.js
+++ src/android/add_application_name.js
@@ -4,8 +4,8 @@ module.exports = function(context) {
 
     var APPLICATION_CLASS = "com.example.template.MyApplication";
 
-    var fs = context.requireCordovaModule('fs'),
-        path = context.requireCordovaModule('path');
+    var fs = require('fs'),
+        path = require('path');
 
     var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
     var manifestFile = path.join(platformRoot, 'AndroidManifest.xml');
